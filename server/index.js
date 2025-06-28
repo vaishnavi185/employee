3e492cb270e768b2a6f3a4c3f59746c3d9622c51
vaishnavi <vishu032004@gmail.com>
2025-06-28 20:40:06 +0530

    starting

--- server/index.js
+++ server/index.js
@@ -1,2 +1,15 @@
 const express = require ('express');
 
+const app=express();
+
+const port =process.env.Port || 3000;
+
+
+app.get('/',(req,res)=>{
+    res.send("hello server")
+})
+
+app.listen(port,()=>{
+    console.log(`server is rumming on port`,port);
+    console.log(`http://localhost:${port}`);
+})
