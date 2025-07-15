d8049eb4eb0e5816a677ab4780e9608e1a811c6b
vaishnavi <vishu032004@gmail.com>
2025-07-15 16:56:02 +0530

    frontend route added

--- empdas/src/App.jsx
+++ empdas/src/App.jsx
@@ -1,23 +1,22 @@
 import { useState } from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import SignupPage from './components/SignupPage'
 import Login from './components/Login'
 import Formm from './components/Formm'
 import ProfileCard from './components/ProfilePage'
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      {/* <SignupPage></SignupPage> */}
-      {/* <Login></Login> */}
-
-      <Formm></Formm>
-      {/* <ProfileCard></ProfileCard> */}
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<SignupPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/form' element={<Formm />} />
+        <Route path='/profile' element={<ProfileCard />} />
+      </Routes>
+    </Router>
   )
 }
 
