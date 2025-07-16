218252abfc8ebb052d56f5929ad57aa4175ac707
vaishnavi <vishu032004@gmail.com>
2025-07-16 13:43:39 +0530

    joined done

--- server/Controllers/ProfileController.js
+++ server/Controllers/ProfileController.js
@@ -1,35 +1,38 @@
 const UserProfile = require('../Model/Userinfo');
-
+const User = require('../Model/data');
 
 const FormController = async(req,res,next)=>{
   const { Fullname, email, phone, Username, Bio,  } = req.body;
   const profilePicture = req.file ? req.file.path : '';
-try{
-    let profile = await UserProfile.findOne({email});
+  try{
+    const user = await User.findOne({email,phone});
 
-    if(profile){
-        profile.Fullname=Fullname;
-        
-        profile.phone=phone;
-        profile.Username=Username;
-        profile.Bio=Bio;
-        if (profilePicture) profile.profilePicture = profilePicture;
-
-    }else{
-        profile=new UserProfile({
-           Fullname,
-        email,
-        phone,
-        Username,
-        Bio,
-        profilePicture
-        })
+    if(!user){
+      return res.status(404).json({message:"user not found"})
     }
-    await profile.save();
-res.status(200).json({ message: 'Profile saved successfully', profile });
-  } catch (err) {
-   console.error(" ERROR in FormController:", err); 
-    res.status(500).json({ message: 'Server error' });
+    let profile = await UserProfile.findOne({userId:user._id})
+if(profile){
+  profile.Fullname=Fullname,
+  profile.Username=Username,
+  profile.Bio=Bio
+  
+  if (profilePicture) profile.profilePicture = profilePicture;
+}
+else{
+  profile =new UserProfile({
+    userId:user._id,
+    Fullname,
+    Username,
+    
+    Bio,
+    profilePicture
+  })
+}
+await profile.save();
+ res.status(200).json({ message: 'Profile saved successfully', profile });
+  }
+  catch(e){
+    console.log(e)
   }
 }
 
--- server/Model/Userinfo.js
+++ server/Model/Userinfo.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
+const User = require('./data')
 
 const profileschema= new mongoose.Schema({
+
+userId :{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:'User',
+     reruired:true,
+     unique:true
+},
+
     Fullname: {type:String ,required:true},
-    email:{type:String,required:true,unique:true},
-    phone:{type:String ,required:true,unique:true},
+    // email:{type:String,required:true,unique:true},
+    // phone:{type:String ,required:true,unique:true},
 Username:{type:String,required:true},
     Bio:{type:String,required:true},
     profilePicture: { type: String, default: '' }, 
